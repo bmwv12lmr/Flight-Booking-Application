696f1aecff9a1365ff41503b572261cba698f6ba
Allen Wu <yanshiun.wu@sjsu.edu>
2022-04-11 14:25:03 -0700

    commandline passed

--- src/main/java/test/RunClient.java
+++ src/main/java/test/RunClient.java
@@ -3,19 +3,20 @@ package test;
 import command.*;
 
 public class RunClient {
-    public static void main(String[] args1) {
-        /*
+    public static void main(String[] args) {
+        
         FileHandler arg1 = new FileHandler(args[0]);
         FileHandler arg2 = new FileHandler(args[1]);
         FileHandler arg3 = new FileHandler(args[2]);
         FileHandler arg4 = new FileHandler(args[3]);
-        */
+        
 
+        /*
         FileHandler arg1 = new FileHandler("/Users/yswu/IdeaProjects/IndividualProject/src/test/Sample.csv");
         FileHandler arg2 = new FileHandler("/Users/yswu/IdeaProjects/IndividualProject/src/test/flights.csv");
         FileHandler arg3 = new FileHandler("/Users/yswu/IdeaProjects/IndividualProject/src/test/play_ground/test.csv");
         FileHandler arg4 = new FileHandler("/Users/yswu/IdeaProjects/IndividualProject/src/test/play_ground/test.txt");
-
+        */
 
 
         Command bookingCSVInitializer = new BookingCSVInitializer(arg1);
