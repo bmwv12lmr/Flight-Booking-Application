c50a2b7905e5d1fe497480e186f30f112b5a43b0
Allen Wu <yanshiun.wu@sjsu.edu>
2022-04-11 14:47:37 -0700

    Remove local comments

--- src/main/java/test/RunClient.java
+++ src/main/java/test/RunClient.java
@@ -9,15 +9,6 @@ public class RunClient {
         FileHandler arg2 = new FileHandler(args[1]);
         FileHandler arg3 = new FileHandler(args[2]);
         FileHandler arg4 = new FileHandler(args[3]);
-        
-
-        /*
-        FileHandler arg1 = new FileHandler("/Users/yswu/IdeaProjects/IndividualProject/src/test/Sample.csv");
-        FileHandler arg2 = new FileHandler("/Users/yswu/IdeaProjects/IndividualProject/src/test/flights.csv");
-        FileHandler arg3 = new FileHandler("/Users/yswu/IdeaProjects/IndividualProject/src/test/play_ground/test.csv");
-        FileHandler arg4 = new FileHandler("/Users/yswu/IdeaProjects/IndividualProject/src/test/play_ground/test.txt");
-        */
-
 
         Command bookingCSVInitializer = new BookingCSVInitializer(arg1);
         Command dbInitializer = new DBInitializer(arg2);
